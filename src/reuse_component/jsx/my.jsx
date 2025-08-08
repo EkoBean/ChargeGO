1503d934201083c3a116bbd6d681a4214f502aea
EkoBean <douzihua02@gmail.com>
2025-08-08 12:58:51 +0800

    vite install

---
+++ src/reuse_component/jsx/my.jsx
@@ -0,0 +1,11 @@
+import NavBar from '../nav.js'
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+    ReactDOM.render(
+        <NavBar />,
+        document.getElementById('root')
+    )
+
+
+document.getElementById('root').innerText = 12345
