d0a9f14e14b9a2865739ae3ced8d6dc838b9a0a4
EkoBean <douzihua02@gmail.com>
2025-08-08 14:30:17 +0800

    Add NavBar component for navigation menu

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,40 @@
-import { StrictMode } from 'react'
+import React, { StrictMode, useEffect } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import '../scss/style.scss';
+import NavBar from './reuse_component/nav.jsx'
 
+
+function App() {
+
+  useEffect(() => {
+    document.getElementById('div1').innerText = 'Click Me(react)';
+    document.querySelector('ul li').style.color = '#FF2051';
+
+
+  }, [])
+  return (
+    <>
+      <NavBar />
+      <div id='div1' className='btn btn-primary'></div>
+      <div
+        class="card text-white bg-primary"
+      >
+        <img class="card-img-top" src="holder.js/100px180/" alt="Title" />
+        <div class="card-body">
+          <h4 class="card-title">Title</h4>
+          <p class="card-text">Text</p>
+        </div>
+      </div>
+
+
+    </>
+  );
+}
+
+
+// ReactDOM.render(<App />,  document.getElementById('root'));  
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
--- src/reuse_component/my.js
+++
@@ -1,6 +0,0 @@
-import NavBar from './nav.js';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { jsx as _jsx } from "react/jsx-runtime";
-ReactDOM.render(/*#__PURE__*/_jsx(NavBar, {}), document.getElementById('root'));
-document.getElementById('root').innerText = 12345;
