36699ccc094710f519e0a1941eeae8345ff2163c
EkoBean <douzihua02@gmail.com>
2025-08-11 14:26:42 +0800

    map api installed

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,13 @@ import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import App from './pages/App';
+import AppIndex from './pages/AppIndex'
 createRoot(document.getElementById('root')).render(
   <Router>
     <Routes>
       <Route path='/App' element={<App />} />
+      <Route path='/AppIndex' element={<AppIndex />} />
     </Routes>
+
   </Router>
 );
