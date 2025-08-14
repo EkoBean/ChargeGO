578841209df9c64f7d5d35319393e7897579d0e9
Kabuto-Lin <lucifer23745787@gmail.com>
2025-08-14 12:36:10 +0800

    newbackv1

---
+++ src/pages/backfront/back/todo-client/src/views/html.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class HtmlView extends Component {
+    state = {  } 
+    render() { 
+        return (<div>HTML View</div>);
+    }
+}
+
+export default HtmlView;
