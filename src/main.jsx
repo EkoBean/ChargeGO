adc5cd0be1087e7fd17bc6ed50a350a4340e73f6
aire-satie <haifans1112@gmail.com>
2025-08-15 15:20:57 +0800

    :(

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React, { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import App from './pages/App';
-createRoot(document.getElementById('root')).render(
-  <Router>
-    <App />
-  </Router>
-);
