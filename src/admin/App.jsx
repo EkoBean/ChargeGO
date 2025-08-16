a435e128097da439c35a1c405970aa2d5f15423a
Kabuto-Lin <lucifer23745787@gmail.com>
2025-08-16 11:18:02 +0800

    backdatav3

--- src/admin/App.jsx
+++ src/admin/App.jsx
@@ -1,20 +1,27 @@
 // import { useState } from 'react'
-import { Routes, Route,Link } from 'react-router-dom';
-import Backhtml from './views/backhtml';
-import Charging from './views/backhtml';
-
+import { Routes, Route } from 'react-router-dom'
+import AdminDashboard from './views/AdminDashboard'
+import Charging from './views/charging'
+// import OrderManagement from './views/OrderManagement'
+// import SiteManagement from './views/SiteManagement'
+// import SystemSettings from './views/SystemSettings'
+// import UserManagement from './views/UserManagement'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
     <>
-     <Routes>
-       <Route path="/" element={<Backhtml />} />
-      <Route path="/backhtml" element={<Backhtml />} />
-      <Route path="/charging" element={<Charging />} />
-     </Routes>
+      <Routes>
+        <Route path="/" element={<AdminDashboard />} />
+        <Route path="/dashboard" element={<AdminDashboard />} />
+        <Route path="/charging" element={<Charging />} />
+        {/* <Route path="/orders" element={<OrderManagement />} /> */}
+        {/* <Route path="/sites" element={<SiteManagement />} /> */}
+        {/* <Route path="/settings" element={<SystemSettings />} /> */}
+        {/* <Route path="/users" element={<UserManagement />} /> */}
+      </Routes>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
-import React, { StrictMode } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import App from './pages/App';
+import { BrowserRouter } from 'react-router-dom'
+import App from './admin/App.jsx' 
+
 createRoot(document.getElementById('root')).render(
-  <Router>
-    <App />
-  </Router>
-);
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+)
