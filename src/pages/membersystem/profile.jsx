1b1c5d0051828613c9aaa07874563aaa39b5065f
aire-satie <haifans1112@gmail.com>
2025-08-25 15:10:54 +0800

    icon edit

--- src/pages/membersystem/profile.jsx
+++ src/pages/membersystem/profile.jsx
@@ -1,3 +1,4 @@
+import { render } from "ejs";
 import React from "react";
 import {useState } from "react";
 import { useNavigate } from "react-router-dom";
@@ -5,5 +6,21 @@ import { useNavigate } from "react-router-dom";
 const Profile = () => {
 
     const [user, setUser] = useState(null);
+
+    render(
+        <div className="profile">
+            {/* 會員資料 */}
+            <div className="title">
+                <img src="./Iconimg/backBtn.svg" alt="返回按鈕" className="back-button" onClick={backBtnClick()}/>
+                <h1>會員資料</h1>
+                <img src="./Iconimg/notify.svg" alt="通知按鈕" className="notify-button" onClick={notifyBtnClick()}/>
+            </div>
+
+            <div className="avatar">
+                <img src="" alt="" />
+            </div>
+
+        </div>
+    )
 
 }
