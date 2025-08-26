757931de2723758919eaaf0c5162671fe7cf00fd
Ranee-chi <ch289266@gmail.com>
2025-08-26 10:47:37 +0800

    新增 NavBarPhone 與 NavBarWebsite 相關檔案

---
+++ src/components/NavBarPhone.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './NavBarPhone.css';
+
+export default function NavBarPhone() {
+    return (
+        <div className="navbar-phone-container">
+            <div className="icon">
+                <img id="myprofile" src="../public/myprofile.png" alt="user" />
+            </div>
+            <div className="icon">
+                <img id="gift" src="../public/gift.png" alt="bell" />
+                {/* <span className="navbar-phone-notification">1</span> */}
+            </div>
+            <div className="icon">
+                <img id="map" src="../public/map.png" alt="energy" />
+            </div>
+            <div className="icon">
+                <img id="CustomerService" src="../public/customer service.png" alt="chat" />
+            </div>
+            <div className="icon">
+                <img id="points" src="/../public/points.png" alt="parking" />
+            </div>
+        </div>
+    );
+}
