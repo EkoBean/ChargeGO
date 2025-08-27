b76c495e65d72364e88d1cb1d09592185d8c59a7
aire-satie <haifans1112@gmail.com>
2025-08-27 16:07:48 +0800

    監修中

---
+++ src/pages/membersystem/mber_rentRecord.jsx
@@ -0,0 +1,18 @@
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const mber_RentRecord = () => {
+
+
+return(
+    <div>
+        <h1>租借紀錄 </h1>
+    </div>
+)
+
+};
+
+
+
+export default mber_RentRecord;
