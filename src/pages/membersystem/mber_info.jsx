c9a0b725d4e7da7cb32e7e904c9d7b666353bc7e
aire-satie <haifans1112@gmail.com>
2025-08-27 16:07:48 +0800

    監修中

---
+++ src/pages/membersystem/mber_info.jsx
@@ -0,0 +1,14 @@
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const mber_Info=()=>{
+
+    return(
+        <div>
+            <h1>會員通知</h1>
+        </div>
+    )
+};
+
+export default mber_Info;
