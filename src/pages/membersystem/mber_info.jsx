4aaef96d445ebc1fd38dbac5c91e719da5f24062
aire-satie <haifans1112@gmail.com>
2025-08-27 16:24:50 +0800

    監修中

--- src/pages/membersystem/mber_info.jsx
+++ src/pages/membersystem/mber_info.jsx
@@ -5,10 +5,25 @@ import { useNavigate } from "react-router-dom";
 const mber_Info=()=>{
 
     return(
-        <div>
-            <h1>會員通知</h1>
+        <div className="mber_info">
+
+        <div className="mber_info_header">
+            <img src="./Iconimg/backBtn.svg"/>
+            <img src="./Iconimg/user.svg"/>
+            <h1>會員名稱</h1>
+            <img src="./Iconimg/notify.svg" />
         </div>
-    )
+        <h2>帳戶通知</h2>
+       <div className="infoBody">
+        <div>存取資料庫notice資料表中的資訊</div>
+        <hr />
+
+       </div>
+
+
+        </div>
+       
+    );
 };
 
 export default mber_Info;
