a724580b50a64e664a28fb11d1af6a62430a97be
Ranee-chi <ch289266@gmail.com>
2025-08-28 15:36:26 +0800

    暫存 App.jsx 的本地修改

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -9,21 +9,23 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // =========== elements ==================
 import NavBar from "../components/nav";
 import AppIndex from "./mapIndex/AppIndex";
-import Register from "./membersystem/register";
-import Login from "./membersystem/login";
+// import Register from "./membersystem/register";
+// import Login from "./membersystem/login";
 import Mission from "./mall/mission";
 import Shop from "./mall/shop";
+import Task from './Task/Task';
 function App() {
   return (
     <>
       <Routes>
+        <Route path="/" element={<Task />} />
         <Route path="/AppIndex" element={<AppIndex />} />
-        <Route path="/Mission" element={<Mission />} />
-        <Route path="/Shop" element={<Shop />}></Route>
-        <Route path="/" element={<WebSite />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="*" element={<Register />} />
+        {/* <Route path="/Mission" element={<Mission />} />
+        <Route path="/Shop" element={<Shop />}></Route> */}
+        {/* <Route path="/" element={<WebSite />} /> */}
+        {/* <Route path="/register" element={<Register />} /> */}
+        {/* <Route path="/login" element={<Login />} />
+        <Route path="*" element={<Register />} /> */}
         {/* 這個以後可以來寫個no Found頁 */}
       </Routes>
     </>
