c84c50da0f1a769ce3dea10384f33023ed5ae5cd
Ranee-chi <ch289266@gmail.com>
2025-08-29 15:19:49 +0800

    同步手機版與桌機版樣式，優化 ChargegoLogo、back-icon、lightning 圖片等

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+import 'animate.css';
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
