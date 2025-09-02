471b8c725581711f797cfd9300f7fe7c9029821c
aire-satie <haifans1112@gmail.com>
2025-09-02 16:23:26 +0800

    CSS調整

--- src/pages/membersystem/mber_profile.jsx
+++ src/pages/membersystem/mber_profile.jsx
@@ -96,82 +96,101 @@ const mber_Profile = () => {
       <ChargegoLogo className="mobile-only-logo" />
       <NavBarPhone />
       {/* Header */}
+      <div className="info-container">
+        <span
+          className="back-icon mobile-only-back"
+          onClick={() => window.history.back()}
+          title="回到上頁"
+        >
+          ◀︎
+        </span>
 
-      <span
-        className="back-icon mobile-only-back"
-        onClick={() => window.history.back()}
-        title="回到上頁"
-      >
-        ◀︎
-      </span>
-      <div className="mobile-arc-bg">
-        <div className="mobile-arc-content">
-          <h2 className="mber_info_title">會員資料</h2>
-        </div>
-      </div>
-      <div className="mber_info_header">
-        <img
-          src="./Iconimg/notify.svg"
-          alt="通知按鈕"
-          className="notify-btn"
-          onClick={notifyBtnClick()}
-        />
-      </div>
-      <div className="mber_info_main">
-        {/* 頭像 */}
-        <div className="avatar">
-          <img src="./Iconimg/user.svg" alt="用戶頭像" />
-        </div>
-        {/* 卡片列 */}
-        <div className="mber_info_cards">
-          <div className="card">
-            <img src="./Iconimg/card_wallet.svg" alt="信用卡資料" />
-            <span>信用卡資料</span>
-          </div>
-          <div className="card">
-            <img src="./Iconimg/card_bill.svg" alt="帳單紀錄" />
-            <span>帳單紀錄</span>
-          </div>
-          <div className="card">
-            <img src="./Iconimg/card_help.svg" alt="幫助中心" />
-            <span>幫助中心</span>
+        <div className="mobile-arc-bg">
+          <div className="mobile-arc-content">
+            <h2 className="mber_info_title">會員資料</h2>
           </div>
         </div>
-        {/* 會員資料區塊 */}
-        <div className="mber_info_profile">
-          <div>
-            <span>會員姓名｜</span>
-            <span>{user?.user_name || "王大明"}</span>
+        <div className="mber_info_header">
+          <img
+            src="./Iconimg/notify.svg"
+            alt="通知按鈕"
+            className="notify-btn"
+            onClick={notifyBtnClick()}
+          />
+        </div>
+        <div className="mber_info_main">
+          {/* 頭像 */}
+          <div className="avatar">
+            <img src="./Iconimg/user.svg" alt="用戶頭像" />
           </div>
-          <div>
-            <span>電話｜</span>
-            <span>
-              {user?.telephone
-                ? user.telephone.replace(/(\d{2})\d{4}(\d{4})/, "$1****$2")
-                : "09**-****-***"}
-            </span>
+          {/* 卡片列 */}
+          <div className="mber_info_cards">
+            <div className="card">
+              <img src="./Iconimg/card_wallet.svg" alt="信用卡資料" />
+              <span>信用卡資料</span>
+            </div>
+            <div className="card">
+              <img src="./Iconimg/card_bill.svg" alt="帳單紀錄" />
+              <span>帳單紀錄</span>
+            </div>
+            <div className="card">
+              <img src="./Iconimg/card_help.svg" alt="幫助中心" />
+              <span>幫助中心</span>
+            </div>
           </div>
-          <div>
-            <span>e-mail｜</span>
-            <span>{user?.email || "gmail@gmail.com"}</span>
-          </div>
-          <div>
-            <span>居住城市｜</span>
-            <select value={country} onChange={handleCountryChange}>
-              <option>台北市</option>
-              <option>新北市</option>
-              <option>桃園市</option>
-              <option>台中市</option>
-              <option>台南市</option>
-              <option>高雄市</option>
-            </select>
+          {/* 會員資料區塊 */}
+          <div className="mber_info_profile">
+            <div>
+              <span>會員姓名｜</span>
+              <span>{user?.user_name || "王大明"}</span>
+            </div>
+            <div>
+              <span>電話｜</span>
+              <span>
+                {user?.telephone
+                  ? user.telephone.replace(/(\d{2})\d{4}(\d{4})/, "$1****$2")
+                  : "09**-****-***"}
+              </span>
+            </div>
+            <div>
+              <span>e-mail｜</span>
+              <span>{user?.email || "gmail@gmail.com"}</span>
+            </div>
+            <div>
+              <span>居住城市｜</span>
+              <select value={country} onChange={handleCountryChange}>
+                <option value="">選擇縣市</option>
+                <option value="台北市">台北市</option>
+                <option value="新北市">新北市</option>
+                <option value="基隆市">基隆市</option>
+                <option value="桃園市">桃園市</option>
+                <option value="新竹縣">新竹縣</option>
+                <option value="新竹市">新竹市</option>
+                <option value="苗栗縣">苗栗縣</option>
+                <option value="台中市">台中市</option>
+                <option value="彰化縣">彰化縣</option>
+                <option value="南投縣">南投縣</option>
+                <option value="雲林縣">雲林縣</option>
+                <option value="嘉義縣">嘉義縣</option>
+                <option value="嘉義市">嘉義市</option>
+                <option value="台南市">台南市</option>
+                <option value="高雄市">高雄市</option>
+                <option value="屏東縣">屏東縣</option>
+                <option value="宜蘭縣">宜蘭縣</option>
+                <option value="花蓮縣">花蓮縣</option>
+                <option value="台東縣">台東縣</option>
+                <option value="連江縣">連江縣</option>
+                <option value="澎湖縣">澎湖縣</option>
+                <option value="金門縣">金門縣</option>
+              </select>
+            </div>
           </div>
         </div>
-      </div>
-      {/* 按鈕區塊 */}
-      <div className="mber_info_btns">
-        <button onClick={handleEditProfile}>修改會員資料</button>
-        <button onClick={handleDeactivateAccount}>會員停權</button>
+        {/* 按鈕區塊 */}
+        <div className="mber_info_btns">
+          <button onClick={handleEditProfile}>修改會員資料</button>
+          <button onClick={handleDeactivateAccount}>會員停權</button>
+        </div>
       </div>
     </div>
   );
