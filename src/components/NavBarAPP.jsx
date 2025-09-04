7415e42ff03b52e32ff037017cfbca306e89bc0f
aire-satie <haifans1112@gmail.com>
2025-09-04 10:44:55 +0800

    ok

--- src/components/NavBarAPP.jsx
+++ src/components/NavBarAPP.jsx
@@ -1,24 +1,12 @@
 import React from 'react';
 import '../styles/scss/NavBarAPP.scss';
 
-<<<<<<< HEAD:src/components/NavBarPhone.jsx
-export default function NavBarPhone() {
-
-    const navigateTo = (path) => {
-       window.location.href = `/${path}`;
-    };
-    
-    return (
-        <div className="navbar-phone-container">
-            <div className="icon" onClick={() => navigateTo("mber_profile")}>
-=======
 export default function NavBarAPP() {
     
     return (
         <div className="navbar-phone-container">
             <div className="icon">
                 <a href="">
->>>>>>> main:src/components/NavBarAPP.jsx
                 <img id="myprofile" src="../public/myprofile.png" alt="user" />
                 </a>
             </div>
--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -6,15 +6,6 @@ import React, { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // =========== elements ==================
-<<<<<<< HEAD
-
-
-import Mber_Register from "./membersystem/mber_register";
-import Mber_Login from "./membersystem/mber_login";
-import Mber_Profile from "./membersystem/mber_profile";
-import Mber_Info from "./membersystem/mber_info";
-import Task from "./membersystem/Task";
-=======
 import NavBar from "../components/nav";
 import MapIndex from "./mapIndex/mapIndex";
 // import Register from "./membersystem/register";
@@ -22,26 +13,18 @@ import MapIndex from "./mapIndex/mapIndex";
 import Mission from "./mall/mission";
 import Shop from "./mall/shop";
 import Login from './Login/Login';
+import Mbre_login from './membersystem/mber_login';
+import Mber_register from './membersystem/mber_register';
 import Coupon from "./mall/coupon";
 import CheckoutCoupons from "./mall/checkout_coupon";
 import MemberLogin from "./MemberLogin";
 import NavbarWebsite from "../components/NavbarWebsite";
 import Task from "./Task";
->>>>>>> main
 
 function App() {
   return (
     <>
       <Routes>
-<<<<<<< HEAD
-        <Route path="/" element={<Mber_Login />} />
-        <Route path="/mber_register" element={<Mber_Register />} />
-        <Route path="/mber_info" element={<Mber_Info />} />
-        <Route path="/mber_login" element={<Mber_Login />} />
-        <Route path="/mber_profile" element={<Mber_Profile />} />
-        <Route path="/task" element={<Task />} />
-        <Route path="*" element={<Mber_Register />} />
-=======
         {/* <Route path="/" element={<Login />} /> */}
         <Route path="/" element={<NavbarWebsite />} />
         <Route path="/MapIndex" element={<MapIndex />} />
@@ -49,21 +32,15 @@ function App() {
         <Route path="/Shop" element={<Shop />}></Route>
         <Route path="/Coupon" element={<Coupon />}></Route>
         <Route path="/CheckoutCoupons" element={<CheckoutCoupons />}></Route>
-        <Route
-          path="/"
-          element={
-            <>
-              <Task />
-            </>
-          }
-        />
+        <Route path="/Task" element={<Task />} />
         {/* <Route path="/register" element={<Register />} /> */}
         {/* <Route path="/login" element={<Login />} /> */}
+        <Route path="/mber_login" element={<Mbre_login />} />
+        <Route path="/mber_register" element={<Mber_register />} />
         <Route path="/MemberLogin" element={<MemberLogin />} />
         <Route path="/Task" element={<Task />} />
         {/* <Route path="*" element={<Register />} /> */}
         {/* 這個以後可以來寫個no Found頁 */}
->>>>>>> main
       </Routes>
     </>
   );
