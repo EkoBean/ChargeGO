e7ffae2f1025a1bb9be08daa38e8fe149beedd59
EkoBean <douzihua02@gmail.com>
2025-09-05 15:02:52 +0800

    NavBarAPP complete

--- src/components/NavBarAPP.jsx
+++ src/components/NavBarAPP.jsx
@@ -4,19 +4,19 @@ import styles from '../styles/scss/NavBarAPP.module.scss';
 export default function NavBarAPP() {
 
     const icons = [
-        { id: styles.myprofile, src: '/myprofile.png', alt: 'user', text: '會員中心' },
-        { id: styles.gift, src: '/gift.png', alt: 'bell', text: '持有優惠卷' },
-        { id: styles.map, src: '/map.png', alt: 'map', text: '回到地圖' },
-        { id: styles.CustomerService, src: '/customer service.png', alt: 'chat', text: '聯絡客服' },
-        { id: styles.points, src: '/points.png', alt: 'parking', text: '點數商城' },
+        { id: styles.myprofile, src: '/myprofile.png', alt: 'user', text: '會員中心', url:'/mber_profile' },
+        { id: styles.gift, src: '/gift.png', alt: 'bell', text: '持有優惠券', url:'/Coupon' },
+        { id: styles.map, src: '/map.png', alt: 'map', text: '回到地圖', url:'/mapIndex' },
+        { id: styles.CustomerService, src: '/customer service.png', alt: 'chat', text: '聯絡客服', url:'' },
+        { id: styles.points, src: '/points.png', alt: 'parking', text: '點數商城', url:'/shop' },
     ];
     return (
         <div className={`${styles.navbarPhoneContainer}`}>
             {icons.map((icon) => (
                 <div key={icon.id} id={icon.id} className={`${styles.icon}`}>
-                    <a href="">
+                    <a href={icon.url} className='text-decoration-none text-dark'>
                         <img src={icon.src} alt={icon.alt} />
-                        <p className='d-none d-md-block text-center'>{icon.text}</p>
+                        <p className='d-none d-md-block text-center text'>{icon.text}</p>
                     </a>
                 </div>
             ))}
