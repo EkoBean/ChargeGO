95c7667012924a0161a68a8b0ae28b4aac5487b2
EkoBean <douzihua02@gmail.com>
2025-09-05 14:28:20 +0800

    starting test reuse compoenent

--- src/pages/mapIndex/mapIndex.jsx
+++ src/pages/mapIndex/mapIndex.jsx
@@ -23,7 +23,7 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 const API_URL = import.meta.env.VITE_BACKEND_API_URL
 
 // testing compoenet
-// import NavBarAPP from '../../components/NavBarAPP';
+import NavBarAPP from '../../components/NavBarAPP';
 
 // ================= Constants ============================
 const APIkey = 'AIzaSyB6R2pe5qFv0A4P2MchR6R9UJ8HpoTVzLg'
@@ -784,7 +784,7 @@ function MapIndex() {
   // ============= Render zone ================
   return (
     <>
-      {/* <NavBarAPP /> */}
+      <NavBarAPP />
       <APIProvider apiKey={APIkey}
         region='TW'
         libraries={['places']}
