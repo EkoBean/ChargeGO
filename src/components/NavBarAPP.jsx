b79723cd0587129a0e6fe3b666528d0c33e7a1fb
EkoBean <douzihua02@gmail.com>
2025-09-05 10:04:49 +0800

    重構 NavBarAPP 組件以使用 icons 陣列，並更新樣式檔案以使用 tokens.module.scss

--- src/components/NavBarAPP.jsx
+++ src/components/NavBarAPP.jsx
@@ -2,27 +2,23 @@ import React from 'react';
 import styles from '../styles/scss/NavBarAPP.module.scss';
 
 export default function NavBarAPP() {
-    
+
+    const icons = [
+        { id: 'myprofile', src: '/myprofile.png', alt: 'user' },
+        { id: 'gift', src: '/gift.png', alt: 'bell' },
+        { id: 'map', src: '/map.png', alt: 'energy' },
+        { id: 'CustomerService', src: '/customer service.png', alt: 'chat' },
+        { id: 'points', src: '/points.png', alt: 'parking' }
+    ]
     return (
         <div className={`${styles.navbarPhoneContainer}`}>
-            <div className={`${styles.icon}`}>
-                <a href="">
-                <img id="myprofile" src="../public/myprofile.png" alt="user" />
-                </a>
-            </div>
-            <div className={`${styles.icon}`}>
-                <img id="gift" src="../public/gift.png" alt="bell" />
-                {/* <span className="navbar-phone-notification">1</span> */}
-            </div>
-            <div className={`${styles.icon}`}>
-                <img id="map" src="../public/map.png" alt="energy" />
-            </div>
-            <div className={`${styles.icon}`}>
-                <img id="CustomerService" src="../public/customer service.png" alt="chat" />
-            </div>
-            <div className={`${styles.icon}`}>
-                <img id="points" src="/../public/points.png" alt="parking" />
-            </div>
-        </div>
+            {icons.map((icon) => (
+                <div key={icon.id} className={`${styles.icon}`}>
+                    <a href="">
+                        <img id={icon.id} src={icon.src} alt={icon.alt} />
+                    </a>
+                </div>
+            ))}
+        </div >
     );
 }
