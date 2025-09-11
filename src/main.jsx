5091061b4155973e4955c54cfc510ecd778a9e55
aire-satie <haifans1112@gmail.com>
2025-09-11 11:42:52 +0800

    開始著手Redis

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import App from "./pages/App";
-import App2 from "./pages/App2";
 import "./styles/scss/global.scss";
 
 createRoot(document.getElementById("root")).render(
